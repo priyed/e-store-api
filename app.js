770cb956bb2586023486e61d4bf23323c9b938d5
priyed <stanleyduye@gmail.com>
2023-02-02 12:36:10 +0100

    added products to mongobd

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 require("dotenv").config();
 // async errors
+require("express-async-errors");
 
 const express = require("express");
 const app = express();
 
-const not_found_middleware = require("./src/middleware/not-found");
-const error_middleware = require("./src/middleware/error-handler");
+const connectDB = require("./src/db/connect");
+const productsRouter = require("./src/routes/products");
+const notFoundMiddleware = require("./src/middleware/not-found");
+const errorMiddleware = require("./src/middleware/error-handler");
 
 // middlware
 app.use(express.json());
@@ -14,17 +17,19 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send('<h1>Store API</h1><a href="/api/v1/products"> products route</a>');
 });
+app.use("/api/v1/products", productsRouter);
 
 const port = process.env.PORT || 8000;
 
 // products route
 
-app.use(not_found_middleware);
-app.use(error_middleware);
+app.use(notFoundMiddleware);
+app.use(errorMiddleware);
 
 const start = async () => {
   try {
     //connectDB
+    await connectDB(process.env.MONGO_URI);
     app.listen(port, console.log(`Server is listening on port ${port}`));
   } catch (error) {
     console.log(error);
---
+++ src/controllers/products.js
@@ -0,0 +1,17 @@
+const Product = require("../models/product");
+
+const getAllProductsStatic = async (req, res) => {
+  const products = await Product.find({
+    featured: true,
+    name: "vase table",
+  });
+  res.status(200).json({ products, nbHits: products.length });
+};
+const getAllProducts = async (req, res) => {
+  res.status(200).json({ mesg: "products route" });
+};
+
+module.exports = {
+  getAllProducts,
+  getAllProductsStatic,
+};
